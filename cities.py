3c53acf9389e7c2fb1c03c8931ed5ecb0aeb7f29
alvaro3dd <101989472+alvaro3dd@users.noreply.github.com>
2024-10-15 14:53:21 -0500

    Update cities.py
    New cities added

--- cities.py
+++ cities.py
@@ -1,65 +1,46 @@
 """
-Oakland California Managed It Services | ok
-Aurora Colorado Managed It Services | ok
-Anaheim California Managed It Services | ok
-Henderson Nevada Managed It Services| ok
-Santa Ana California Managed It Services
-Anchorage Alaska Managed It Services | No
-Chula Vista California Managed It Services | ok
-Reno Nevada Managed It Services
-Glendale Arizona Managed It Services
-Boise Idaho Managed It Services
-Spokane Washington; Managed It Services
-San Bernardino California; Managed It Services
-Modesto California; Managed It Services
-Fontana California; Managed It Services
-Salt Lake City; Managed It Services
-Glendale California Managed It Services
-Vancouver Washington Managed It Services
-Santa Rosa California Managed It Services
-Elk Grove California Managed It Services
-Ontario California; Managed It Services
-Oceanside California; Managed It Services
-Garden Grove California; Managed It Services
-Palmdale California; Managed It Services
-Hayward California; Managed It Services
-Lakewood Colorado; Managed It Services
-Bellevue Washington; Managed It Services
-Escondido California; Managed It Services
-Torrance California; Managed It Services
-Surprise Arizona; Managed It Services
-Visalia California; Managed It Services
-Orange California; Managed It Services
-Kent Washington; Managed It Services
-Santa Clara California; Managed It Services
-Simi Valley California; Managed It Services
-Concord California; Managed It Services
-Berkeley California; Managed It Services
-Fairfield California; Managed It Services
-Billings Montana Managed; It Services
-Richmond California Managed; It Services
-Antioch California; Managed It Services
-Carlsbad California; Managed It Services
-Gresham Oregon Managed; It Services
-Pueblo Colorado Managed; It Services
-Murrieta California Managed; It Services
-Everett Washington Managed; It Services
-
-
-Santa Maria California; Managed It Services
-El Monte California; Managed It Services
-Sparks Nevada; Managed It Services
-Boulder Colorado; Managed It Services
-Burbank California; Managed It Services
-
-Hillsboro Oregon; Managed It Services
-San Mateo California; Managed It Services
-Rio Rancho New Mexico; Managed It Services
-Spokane Valley Washington; Managed It Services
-Vacaville California; Managed It Services
-Nampa Idaho; Managed It Services
-Bend Oregon; Managed It Services
-St. George Utah; Managed It Services
-Buckeye Arizona; Managed It Services>
+Dallas Texas; Managed Service Provider
+Austin Texas; Managed Service Provider
+Fort Worth Texas; Managed Service Provider
+El Paso Texas; Managed Service Provider
+Arlington Texas; Managed Service Provider
+Corpus Christi Texas; Managed Service Provider
+Plano Texas; Managed Service Provider
+Lubbock Texas; Managed Service Provider
+Laredo Texas; Managed Service Provider
+Irving Texas; Managed Service Provider
+Garland Texas; Managed Service Provider
+Frisco Texas; Managed Service Provider
+McKinney Texas; Managed Service Provider
+Amarillo Texas; Managed Service Provider
+Grand Prairie Texas; Managed Service Provider
+Brownsville Texas; Managed Service Provider
+Killeen Texas; Managed Service Provider
+Denton Texas; Managed Service Provider
+Mesquite Texas; Managed Service Provider
+Pasadena Texas; Managed Service Provider
+McAllen Texas; Managed Service Provider
+Waco Texas; Managed Service Provider
+Midland Texas; Managed Service Provider
+Lewisville Texas; Managed Service Provider
+Carrollton Texas; Managed Service Provider
+Round Rock Texas; Managed Service Provider
+Abilene Texas; Managed Service Provider
+Pearland Texas; Managed Service Provider
+College Station Texas; Managed Service Provider
+Richardson Texas; Managed Service Provider
+League City Texas; Managed Service Provider
+Odessa Texas; Managed Service Provider
+Beaumont Texas; Managed Service Provider
+Allen Texas; Managed Service Provider
+New Braunfels Texas; Managed Service Provider
+Tyler Texas; Managed Service Provider
+Sugar Land Texas; Managed Service Provider
+Conroe Texas; Managed Service Provider
+Edinburg Texas; Managed Service Provider
+Wichita Falls Texas; Managed Service Provider
+Milwaukee Texas; Managed Service Provider
+Madison Texas; Managed Service Provider
+Green Bay Texas; Managed Service Provider
 
 """
